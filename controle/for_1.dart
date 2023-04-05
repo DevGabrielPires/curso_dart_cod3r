82a77f1e4dece1f2640dcc4c821ebc643c3cbefd
devgabrielpires <gabriel.r.pires@outlook.com>
2023-04-05 14:59:48 -0300

    :floppy_disk: Save: finishing class about for

--- controle/for_1.dart
+++ controle/for_1.dart
@@ -1,18 +1,39 @@
+// void main(List<String> args) {
+//   for (var i = 0; i <= 10; i++) {
+//     print('i = $i');
+//   }
+//   print('fim do primeiro \n\n');
+
+//   for (var i = 100; i >= 0; i -= 4) {
+//     print('i = $i');
+//   }
+//   print('fim do segundo \n\n\n');
+//   int b = 0;
+//   for (; b <= 10; b++) {
+//     print('b = $b');
+//   }
+
+//   print('[FORA] b = $b');
+//   print('fim');
+// }
+
+import 'dart:math';
+
 void main(List<String> args) {
-  for (var i = 0; i <= 10; i++) {
-    print('i = $i');
+  List<int> idades = [];
+  for (var i = 0; i < 30; i++) {
+    idades.add(Random().nextInt(101));
   }
-  print('fim do primeiro \n\n');
+  print(idades);
+  print(idades.length);
 
-  for (var i = 100; i >= 0; i -= 4) {
-    print('i = $i');
-  }
-  print('fim do segundo \n\n\n');
-  int b = 0;
-  for (; b <= 10; b++) {
-    print('b = $b');
-  }
+  var maior = 0;
 
-  print('[FORA] b = $b');
-  print('fim');
+  idades.forEach((element) {
+    if (element > maior) {
+      maior = element;
+    }
+  });
+
+  print('A maior idade é $maior');
 }
