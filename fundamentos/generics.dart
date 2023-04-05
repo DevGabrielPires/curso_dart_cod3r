e10598616d8cfe88957be1be4d2098e9f967581c
devgabrielpires <gabriel.r.pires@outlook.com>
2023-04-05 11:16:50 -0300

    :floppy_disk: Save: finishing class about generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  List<String> listaDeFrutas = ['Banana', 'Maça', 'Morango', 'Laranja'];
+  listaDeFrutas.add('Melão');
+  print(listaDeFrutas);
+
+  Map<String, double> salarios = {
+    'Gerente': 123123.23,
+    'vendedor': 13123.43,
+    'estágiario': 123.63,
+    // 'Gerente': 23.23, substitui o valor
+  };
+
+  print(salarios);
+}
